8d8d2171e489170b80c573315d691d86be8ef303
BaTung <baztung@gmail.com>
2023-04-17 22:21:04 +0700

    03 17/04/2023 Tung thêm Firebase_core , firebase_Auth, test connect o home.dart getUser() Text("$user"

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+// import 'package:moneyapp/screens/home.dart';
+import 'package:moneyapp_v1/screens/home.dart';
+import 'package:moneyapp_v1/screens/statistics.dart';
+import '../component/bottomnavigationbar.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+
+import 'data/model/add_date.dart';
+//Firebase
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+
+
+void main() async {
+  await Hive.initFlutter();
+  Hive.registerAdapter(AdddataAdapter());
+  await Hive.openBox<Add_data>('data');
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Bottom(),
+    );
+  }
+}
